6964426d5c223998631305f2f050e5280aa2cb74
Himanshi Gupta <himanshigupta8899@gmail.com>
2025-11-17 23:00:14 +0530

    some more

--- array.py
+++ array.py
@@ -330,3 +330,32 @@ def longest_consecutive(nums):
 def is_palindrome(s):
     cleaned="".join(c.lower() for c in s if c.isalnum())
     return cleaned==cleaned[::-1]
+
+def is_plaindrome(s):
+    left,right=0,len(s)-1
+    while left<right:
+        while left< right and not s[left].isalnum():
+            left+=1
+        while left< right and not s[right].isalnum():
+            right-=1
+        if s[left].lower()!=s[right].lower():
+            return False
+        left+=1
+        right-=1
+    return True
+print(is_palindrome("A man, a plan, a canal: Panama"))
+# Time: O(n) • Space: O(1)
+
+# two sum II - Input array is sorted
+def two_sum_sorted(number,target):
+    left,right=0,len(number)-1
+    while left<right:
+        current_sum=number[left]+number[right]
+        if current_sum==target:
+            return[left+1,right+1]
+        elif current_sum<target:
+            left+=1
+        else:
+            right-=1
+
+
